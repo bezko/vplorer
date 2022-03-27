d8fc3eaec45613aa7462498b97ff8a6f23f6fb09
bezko <mathieu@bezko.org>
2022-03-27 19:14:27 -0400

    expandable folders

--- src/Item.vue
+++ src/Item.vue
@@ -1,10 +1,12 @@
 <template>
  <ul>
-     <li v-for="e in content">
-      <div v-if="typeof e == 'string'">{{e}}</div>
+     <li v-for="(e,index) in content">
+      <div v-if="typeof e == 'string'" >{{e}}</div>
       <div v-if="typeof e == 'object'">
-        <div class="folder_name">{{e.name}}/</div>
-        <Item :content="e.content"></Item>
+        <div class="folder_name"  @click="this.expanded[index] = !this.expanded[index]">{{e.name}}/</div>
+        <div v-bind:class="{ hidden: !expanded[index] }">
+            <Item :content="e.content"  ></Item>
+        </div>
       </div>
      </li>
  </ul>
@@ -12,10 +14,18 @@
 <script>
 export default {
   name: 'Item',
-  props: ['content']
+  props: ['content'],
+  data()
+  {
+      return {expanded:[]}
+  }
 }
 </script>
 <style scoped>
+.hidden
+{
+    display: none;
+}
 ul
 {
     padding-inline-start: 10px;
